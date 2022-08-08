730c1272c0293db38a557ba00d2a2be6410a5984
xoraus <imsafay@gmail.com>
2022-08-08 14:20:31 +0530

    7. Functions

---
+++ Javascript Fundamentals/7. Functions.js
@@ -0,0 +1,11 @@
+// function greet() {
+//     console.log('Hello World!');
+// }
+
+// greet();
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('xoraus', 'codes');
