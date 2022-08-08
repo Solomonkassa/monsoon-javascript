58a3557880ddf1028ba237b84aafccafea1dae50
xoraus <imsafay@gmail.com>
2022-08-08 14:14:59 +0530

    6. Arrays

---
+++ Javascript Fundamentals/6. Arrays.js
@@ -0,0 +1,20 @@
+let selectedColors = ['red', 'Blue'];
+console.log(selectedColors);
+
+// accessing array elements
+
+console.log(selectedColors[0]);
+console.log(selectedColors[1]);
+
+selectedColors[2] = 'greeen';
+console.log(selectedColors[2]);
+
+// finding type of selectedColors
+
+console.log(typeof (selectedColors));
+
+// technically an array is an object
+
+console.log(selectedColors.length);
+
+// An array is a data structure which can be used to represent list of items.
