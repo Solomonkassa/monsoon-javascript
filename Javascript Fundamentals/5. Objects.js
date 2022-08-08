5fe9f71e61260ca5a72814db061fa8f15cbcafe1
xoraus <imsafay@gmail.com>
2022-08-08 14:06:49 +0530

    5. Objects

---
+++ Javascript Fundamentals/5. Objects.js
@@ -0,0 +1,36 @@
+/*
+let name = 'xoraus';
+let age = 25;
+*/
+
+// above can be replaced with person object.
+
+let person = {
+    // properties or keys
+    name: 'xoraus',
+    age: 25
+};
+
+console.log(person);
+
+// Dot Notations
+person.name = 'forestwolf'
+
+console.log(person.name)
+
+// Bracket Notations
+
+person['name'] = 'forestfox';
+
+console.log(person.name);
+
+// more advance bracket notation
+
+let selection = 'name';
+
+person[selection] = 'bear';
+
+console.log(person.name);
+
+
+
