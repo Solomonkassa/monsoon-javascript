21f18ae663048977c39c3ea8dbd0f5b1615cd305
xoraus <imsafay@gmail.com>
2022-08-08 17:39:07 +0530

    Level 1 Exercises Completed

---
+++ 30 Days of Code Exercises/2. Data Type - Exercise.js
@@ -0,0 +1,116 @@
+// Declare a variable named challenge and assign it to an initial value **'30 Days Of JavaScript'**.
+
+const { Console } = require("console");
+
+let challenege = '30 Days Of JavaScript';
+
+// Print the string on the browser console using **console.log()
+
+console.log(challenege);
+
+// Print the **length** of the string on the browser console using _console.log()_
+
+console.log(challenege.length);
+
+// Change all the string characters to capital letters using **toUpperCase()** method
+
+console.log(challenege.toUpperCase());
+
+//  Change all the string characters to lowercase letters using **toLowerCase()** method
+
+console.log(challenege.toLowerCase());
+
+//  Cut (slice) out the first word of the string using **substr()** or **substring()** method
+
+console.log(challenege.substring(0, 2));
+
+//  Slice out the phrase _Days Of JavaScript_ from _30 Days Of JavaScript_.
+
+console.log(challenege.substring(3, 21));
+
+// Check if the string contains a word **Script** using **includes()** method
+
+console.log(challenege.includes('Script'));
+
+//  Split the **string** into an **array** using **split()** method
+
+console.log(challenege.split());
+
+// Split the string 30 Days Of JavaScript at the space using **split()** method
+
+console.log(challenege.split(' '));
+
+//  'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' **split** the string at the comma and change it to an array.
+
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+console.log(companies.split(','));
+
+// Change 30 Days Of JavaScript to 30 Days Of Python using **replace()** method.
+
+console.log(challenege.replace('JavaScript', 'Python'));
+
+//  What is character at index 15 in '30 Days Of JavaScript' string? Use **charAt()** method.
+
+console.log(challenege.charAt(15));
+
+//  What is the character code of J in '30 Days Of JavaScript' string using **charCodeAt()**
+
+console.log(challenege.charCodeAt('J'));
+
+//  Use **indexOf** to determine the position of the first occurrence of **a** in 30 Days Of JavaScript
+
+console.log(challenege.indexOf('a'));
+
+//  Use **lastIndexOf** to determine the position of the last occurrence of **a** in 30 Days Of JavaScript.
+
+console.log(challenege.lastIndexOf('a'));
+
+// Use **indexOf** to find the position of the first occurrence of the word **because** in the following sentence:**'You cannot end a sentence with because because because is a conjunction'**
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+
+console.log(sentence.indexOf('because'));
+
+// Use **lastIndexOf** to find the position of the last occurrence of the word **because** in the following sentence:**'You cannot end a sentence with because because because is a conjunction'**
+
+console.log(sentence.lastIndexOf('because'));
+
+// Use **search** to find the position of the first occurrence of the word **because** in the following sentence:**'You cannot end a sentence with because because because is a conjunction'**
+
+console.log(sentence.search('because'));
+
+// Use **trim()** to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+
+let spaces = '  wow lots of spaces  ';
+
+console.log(spaces.trim());
+
+
+//  Use **startsWith()** method with the string _30 Days Of JavaScript_ and make the result true
+
+console.log(challenege.startsWith('30'));
+
+// Use **endsWith()** method with the string _30 Days Of JavaScript_ and make the result true
+
+console.log(challenege.endsWith('JavaScript'));
+
+// Use **match()** method to find all the **a**’s in 30 Days Of JavaScript
+
+let string = 'a';
+let pattern = /a/gi
+
+console.log(challenege.match(pattern));
+
+// Use **concat()** and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+
+let stringOne = '30 Days of';
+let stringTwo = ' JavaScript';
+
+let fullString = stringOne + stringTwo;
+
+console.log(fullString);
+
+// Use **repeat()** method to print 30 Days Of JavaScript 2 times
+
+console.log(challenege.repeat(2));
