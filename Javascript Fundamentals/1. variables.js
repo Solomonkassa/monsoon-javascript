1d990df8ca0f815d848c5fa9f89be1e2eecf8961
xoraus <imsafay@gmail.com>
2022-08-08 13:05:36 +0530

    1.variables & 2.constants

---
+++ Javascript Fundamentals/1. variables.js
@@ -0,0 +1,17 @@
+let myName = 'xoraus';
+console.log(myName);
+
+/* Rules for Vairables
+    Cannot be a reserved keyword
+    Should be meaningful
+    Cannot start with a number (1name)
+    Cannot contain a space or hyphen (-)
+    Are case-sensitive
+*/
+
+let firstName = 'Forest';
+let lastName= 'Fox';
+
+
+
+
---
+++ Javascript Fundamentals/2. constants.js
@@ -0,0 +1,5 @@
+// constatns cannot be reassigned
+
+const interestRate = 0.3;
+interestRate = 1; // this will throw an error
+console.log(interestRate);
