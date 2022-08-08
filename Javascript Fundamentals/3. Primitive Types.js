c9704678b3ebad226c2eab21cac4dd93ff1add7c
xoraus <imsafay@gmail.com>
2022-08-08 13:10:49 +0530

    3. primitive types

---
+++ Javascript Fundamentals/3. Primitive Types.js
@@ -0,0 +1,17 @@
+// Primitives or Value Types
+
+/*
+String
+Number
+Boolean
+undefined
+null
+*/
+
+let name = 'xoraus'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined; // if declared just firstName; then it's undefined by default
+let selectedColor = null; // null in situations where we want to clear the value of a variable. (eg. from color red to null)
+
+
